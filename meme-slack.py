95f847cbe6d97494cf1a2bb72e89f07cd3b9f211
Sam Collins <sam@scollins.co>
2017-08-11 00:25:20 +0100

    Switched to simple db based memes

--- meme-slack.py
+++ meme-slack.py
@@ -1,56 +1,64 @@
 import os
-import random
+from random import randint
 import json
-from flask import Flask, jsonify
+import sqlite3
+
+from flask import Flask, jsonify, g, request
 from slackclient import SlackClient
 
 app = Flask(__name__)
 
-filepath = '/home/scollins/PycharmProjects/meme-slack/memes.txt'
-users = {}
-slack_token = os.environ.get('SLACK_API_TOKEN',
-                             'xoxp-165009029618-165009030770-225009348531-2a387c6033cfdc106b8e59ab88aadb3b')
+DATABASE = '/home/scollins/PycharmProjects/meme-slack/memes.db'
+TOKEN = 'xoxp-165009029618-165009030770-225009348531-2a387c6033cfdc106b8e59ab88aadb3b'
 
 
-def find_user(id):
-    if id in users:
-        return users[id]
-    client = SlackClient(slack_token)
-    user = client.api_call('users.info', user=id)['user']
-    print(user)
-    users[id] = {
-        'id': user['id'],
-        'name': user['real_name'],
-        'avatar': user['profile']['image_512']
-    }
-    return users[id]
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 
-def return_message(meme):
-    return jsonify({
-        'time': meme['time'],
-        'text': meme['text'],
-        'user': find_user(meme['user']),
-    })
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 
-@app.route('/')
-def status():
-    return 'Found meme file %s' % os.path.getsize(filepath)
+def update_user(id):
+    pass
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/random')
-def random_message():
-    with open(filepath) as file:
-        meme = json.loads(random.choice(file.read().splitlines()))
-    return return_message(meme)
+def random():
+    total = query_db("SELECT count(*) FROM messages", one=True)
+    id = randint(1, total[0])
+    return jsonify(
+        query_db("SELECT * FROM messages WHERE id = ?", [id], one=True))
 
 
 @app.route('/latest')
-def latest_message():
-    with open(filepath) as file:
-        meme = json.loads(file.readlines()[-1])
-    return return_message(meme)
+def latest():
+    return jsonify(
+        query_db("SELECT * FROM messages ORDER BY ts DESC LIMIT 1", one=True))
+
+
+@app.route('/slack/event')
+def event():
+    event = request.get_json()
+    if event['event']['type'] == 'message':
+        message = event['event']
+        query_db("INSERT INTO messages VALUES (NULL, ?, ?, ?, ?)",
+                 [message['channel'], message['user'], message['text'],
+                  message['ts']])
+        update_user(message['user'])
 
 
 if __name__ == '__main__':
---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE messages
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    channel TEXT,
+    user TEXT,
+    text TEXT,
+    ts TEXT
+);
